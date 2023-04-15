5af274abb1e948cecf585f291ad0981ac4639a03
SadJoeBright <sadjoebright.gmail.com>
2023-04-16 01:09:28 +0300

    feat: several tasks solved

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string") {
+    return false;
+  }
+  const numericActivity = parseFloat(sampleActivity);
+  if (isNaN(numericActivity) || numericActivity <= 0 || numericActivity > MODERN_ACTIVITY) {
+    return false;
+  }
+  const age = Math.ceil(Math.log(MODERN_ACTIVITY / numericActivity) * HALF_LIFE_PERIOD / 0.693);
+  return age;
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -9,13 +9,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
- * depthCalc.calculateDepth([[[]]]) => 3
- *
+ * depthCalc.calculateDepth([ [ [] ] ]) => 3
+ [1, 2, [3, 4, [5]], 6]
+ [1, 2, 3, 4, [5], 6]
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let depth = 1;
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        depth += this.calculateDepth(arr.flat());
+        return depth
+      }
+    }
+    return depth
   }
 }
 
